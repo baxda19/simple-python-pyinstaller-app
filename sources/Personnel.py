51be9fd2a0109323673bbfdae1394f41d7d4b129
baxda19 <jacobbaxterdavis@gmail.com>
2019-07-31 23:42:39 +1000

    dumbass

--- sources/Personnel.py
+++ sources/Personnel.py
@@ -2,4 +2,4 @@ import pyautogui
 
 file = input("Press enter CSV file name:")
 
-alert(text='kek', title='omg it huh not', button='OK')
+pyautogui.alert(text='kek', title='omg it huh not', button='OK')
