fd1b30505f3fa45d95a3f806075b260ff0968edd
baxda19 <jacobbaxterdavis@gmail.com>
2019-07-31 23:52:36 +1000

    come on baby

--- sources/Personnel.py
+++ sources/Personnel.py
@@ -1,5 +1,7 @@
 import pyautogui
+import pyperclip
 
 file = input("Press enter CSV file name:")
 
 pyautogui.alert(text='kek', title='omg it huh not', button='OK')
+pyperclip.copy('You did it bitch')
